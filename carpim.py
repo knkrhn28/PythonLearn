80db26553541431537a48591148169bd4073671c
knkrhn28 <oqan_28@hotmail.com>
2017-03-11 14:10:33 +0300

    Çarpım Tablosu For Döngüsü

---
+++ carpim.py
@@ -0,0 +1,12 @@
+'''
+OkanKARAHAN
+Çarpým Tablosu
+'''
+# -*- coding: cp1254 -*-
+for i in range(1,11,1):
+           for j in range(1,11):
+                      k=i*j
+                      print  str(i) +"*"+str(j)+"=",str(k)
+           print
+
+#Saygýlar.....
