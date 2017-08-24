33bc3262f649c4918bf375de04ccc9a448331f1f
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-25 00:10:41 +0300

    baklavaYap

--- baklava.py
+++ baklava.py
@@ -1,4 +1,4 @@
-def baklavayap(n):
+def baklavaYap(n):
     s=""
     for i in range(1,n+1):
         for k in range(n-i):
@@ -14,4 +14,4 @@ def baklavayap(n):
             s=s+"*"
         print s
         s=""
-baklavayap(int(raw_input("N sayisini Giriniz: ")))
+baklavaYap(int(raw_input("N sayisini Giriniz: ")))
