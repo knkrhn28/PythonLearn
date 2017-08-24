c63e266e7ba4da5ded8ff765ad2671819a9f74e0
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-24 23:55:33 +0300

    asalSayilar

---
+++ asal.py
@@ -0,0 +1,10 @@
+son=int(raw_input("Ust Sinir: "))
+liste=[]
+for i in range(2,son):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        liste.append(i)
+print liste
+            
