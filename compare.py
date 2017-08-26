bc2eafe77d200b01e8a9a5c2d7b361c424a6ab59
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-26 08:34:12 +0300

    Compare

---
+++ compare.py
@@ -0,0 +1,10 @@
+def compare(x,y):
+    if x < y:
+        print x, "küçüktür", y
+    elif x > y:
+        print x, "büyüktür", y
+    else:
+        print x, "ve", y, "eþittir"
+compare(7,7)
+compare(6,100)
+compare(51,5)
